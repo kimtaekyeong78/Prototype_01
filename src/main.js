89d23d392483fc74d1444da0105b2bbf0895cd54
tkkim <tkkim@eduwill.net>
2021-11-12 18:16:49 +0900

    modify

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import BaseInput from '@/components/BaseInput';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.component('Baseinput', BaseInput);
+app.use(store).use(router).mount('#app');
